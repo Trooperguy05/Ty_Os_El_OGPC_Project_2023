97e0d2ebb17df1c07961072bbc20b9e33249f83d
Trooperguy05 <87686318+Trooperguy05@users.noreply.github.com>
2023-02-18 01:49:30 -0800

    Some changes

---
+++ Ty_Os_Personal_Project2/Assets/Scripts/Player Scripts/Flashlight.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    private Light light; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void
+}
