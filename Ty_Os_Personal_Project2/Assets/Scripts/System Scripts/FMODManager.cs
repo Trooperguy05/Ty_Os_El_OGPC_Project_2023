fdb0b01a30755fedb24e11d69b9ae0a623648246
Trooperguy05 <Tyry2005@outlook.com>
2023-02-22 09:46:37 -0800

    FMOD Manager
    Added FMOD Manager script to help with loading and unloading FMOD banks and fixed some issues within FMOD

---
+++ Ty_Os_Personal_Project2/Assets/Scripts/System Scripts/FMODManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FMODManager : MonoBehaviour
+{
+    [Header("FMOD Bank References")]
+    [FMODUnity.BankRef]
+    public string playerSFX;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // Load the playerSFX FMOD bank on awake \\
+        FMODUnity.RuntimeManager.LoadBank(playerSFX, true);
+    }
+
+    // Loads a specified FMOD bank \\
+    public IEnumerator LoadBank(string bank) {
+        FMODUnity.RuntimeManager.LoadBank(bank);
+        yield return null;
+    }
+    
+    // Overload coroutine to accept a list \\
+    public IEnumerator LoadBank(List<string> banks) {
+        foreach (string bank in banks){
+            FMODUnity.RuntimeManager.LoadBank(bank);
+            yield return null;
+        }
+    }
+
+    // Unloads a specified FMOD bank \\
+    public IEnumerator UnloadBank(string bank) {
+        FMODUnity.RuntimeManager.UnloadBank(bank);
+        yield return null;
+    }
+
+    // Overload coroutine to accept a list \\
+    public IEnumerator UnloadBank(List<string> banks) {
+        foreach (string bank in banks) {
+            FMODUnity.RuntimeManager.UnloadBank(bank);
+            yield return null;
+        }
+    }
+}
