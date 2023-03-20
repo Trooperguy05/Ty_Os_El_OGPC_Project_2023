157e7427083d1df450b65480bdad4fe555556043
EatBugz <77813942+EatBugz@users.noreply.github.com>
2023-03-20 13:49:09 -0700

    pain
    pain

--- Ty_Os_Personal_Project2/Assets/Scripts/ExitPedestal.cs
+++ Ty_Os_Personal_Project2/Assets/Scripts/ExitPedestal.cs
@@ -2,14 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExitPedestal : MonoBehaviour
+public class ExitPedestal : Interactable
 {
-    public int charge;
-    private GameObject exitDoors;
-    private PlayerInteract pI;
-
-    void Start() {
-        pI = GameObject.Find("Player").GetComponent<PlayerInteract>();
-        exitDoors = GameObject.Find("Exit Doors");
+    protected override void Interact() {
+        Debug.LogError("pain");
     }
 }
