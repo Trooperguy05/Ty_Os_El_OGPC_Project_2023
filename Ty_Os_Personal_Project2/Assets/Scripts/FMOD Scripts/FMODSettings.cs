793d489778ae2ce9a1e6a35483196bdcecb876ad
Trooperguy05 <87686318+Trooperguy05@users.noreply.github.com>
2023-03-27 07:49:13 -0700

    Create FMODSettings.cs

---
+++ Ty_Os_Personal_Project2/Assets/Scripts/FMOD Scripts/FMODSettings.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODSettings : MonoBehaviour
+{
+    // Audio settings to transer between scenes \\
+    [Header("Volumes")]
+    public float masterVolume;
+    public float playerVolume;
+    public float monsterVolume;
+    public float ambienceVolume;
+}
