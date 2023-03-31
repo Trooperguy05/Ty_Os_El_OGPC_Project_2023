ac219d7df46d0e3a3393d130f030e318c5f774ca
Trooperguy05 <Tyry2005@outlook.com>
2023-03-31 09:03:41 -0700

    Credits button on the main menu

--- Ty_Os_Personal_Project2/Assets/Scripts/Credits.cs
+++ Ty_Os_Personal_Project2/Assets/Scripts/Credits.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class Credits : MonoBehaviour
 {
     public SceneLoader sL;
-    public void switchScene() {
+    
+    public void loadMainMenu() {
         StartCoroutine(sL.loadScene(0));
+    }
+
+    public void loadCredits() {
+        StartCoroutine(sL.loadScene(4));
     }
 }
